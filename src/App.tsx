627e7df3c78425d79a08afded9fc783ad9e86a10
dacardenasa <diego.cardenasaleg@gmail.com>
2022-03-19 22:40:16 -0500

    Added logic to handle components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import { useEffect, useState, createContext } from 'react';
+import { IRootBike, IContextProps, IBike } from '@/interfaces/bikes.interface';
+import { Layout, SearchForm, BikesCases } from '@/components';
+import { fetchAPI } from '@/services/api.search.service';
+
 import './App.css';
-import { Layout } from './components/templates';
 import './Global.css';
 
+export const MyContext = createContext<IContextProps>({});
+
 function App() {
+  const [bikesData, setBikesData] = useState<IRootBike[]>([]);
+  const [isFetchingData, setIsFetchingData] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsFetchingData((c) => !c);
+    fetchAPI({ location: 'berlin' }).then((response) => {
+      setIsFetchingData((c) => !c);
+      setBikesData(response);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <Layout>
-        <p>Hello World!</p>
-      </Layout>
-    </div>
+    <MyContext.Provider
+      value={{
+        bikesCases: bikesData,
+        handleBikesData: (data: IRootBike[]) => setBikesData(data),
+        isFetchingData,
+        handleFetchingData: () => setIsFetchingData((c) => !c),
+      }}
+    >
+      <div className="App">
+        <Layout>
+          <SearchForm />
+          <BikesCases />
+        </Layout>
+      </div>
+    </MyContext.Provider>
   );
 }
 
